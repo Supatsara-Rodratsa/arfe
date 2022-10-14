1808c3586e9a7153a26e544cbd7ac47b839d0942
supatsara <supatsara.rodratsa@gmail.com>
2022-10-14 11:34:08 +0200

    email validation

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -20,7 +20,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginScreenComponent implements OnInit {
 
   public form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
   });
 
